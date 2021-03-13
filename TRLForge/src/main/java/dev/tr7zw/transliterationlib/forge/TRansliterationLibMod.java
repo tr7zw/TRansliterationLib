968ac736d53f8bac3b95656e3e383d12423b7ee1
tr7zw <tr7zw@live.de>
2021-03-13 23:05:58 +0100

    Adding clientside tag and fix Forge server mod compare check

--- TRLForge/src/main/java/dev/tr7zw/transliterationlib/forge/TRansliterationLibMod.java
+++ TRLForge/src/main/java/dev/tr7zw/transliterationlib/forge/TRansliterationLibMod.java
@@ -1,8 +1,13 @@
 package dev.tr7zw.transliterationlib.forge;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 import dev.tr7zw.transliterationlib.api.TRansliterationLib;
 import dev.tr7zw.transliterationlib.api.event.APIEvent;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Mod(TRansliterationLibMod.MODID)
 public class TRansliterationLibMod {
@@ -12,6 +17,8 @@ public class TRansliterationLibMod {
 	public TRansliterationLibMod() {
 		TRansliterationLib.transliteration = new TRansliterationLibAPIImpl();
 		APIEvent.LOADED.invoker().run();
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST,
+				() -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (remote, isServer) -> true));
 	}
 
 }
