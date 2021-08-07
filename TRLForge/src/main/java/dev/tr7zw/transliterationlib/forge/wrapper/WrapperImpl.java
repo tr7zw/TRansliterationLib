984e5f758bee5937103e27b5251bd77d9c5e181e
tr7zw <tr7zw@live.de>
2021-08-07 16:58:51 +0200

    Patch together the map render method

--- TRLForge/src/main/java/dev/tr7zw/transliterationlib/forge/wrapper/WrapperImpl.java
+++ TRLForge/src/main/java/dev/tr7zw/transliterationlib/forge/wrapper/WrapperImpl.java
@@ -1,8 +1,11 @@
 package dev.tr7zw.transliterationlib.forge.wrapper;
 
 import java.util.function.BiConsumer;
+import java.util.function.Function;
 
-import com.mojang.blaze3d.vertex.IVertexBuilder;
+import com.mojang.blaze3d.vertex.DefaultVertexFormat;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexFormat;
 import com.mojang.math.Matrix4f;
 import com.mojang.math.Vector3f;
 
@@ -14,26 +17,25 @@ import dev.tr7zw.transliterationlib.api.wrapper.WrappedText;
 import dev.tr7zw.transliterationlib.api.wrapper.item.ItemStack;
 import dev.tr7zw.transliterationlib.api.wrapper.util.MatrixStack;
 import dev.tr7zw.transliterationlib.api.wrapper.util.VertexConsumerProvider;
-import dev.tr7zw.transliterationlib.forge.RenderPhaseAlternative;
 import dev.tr7zw.transliterationlib.forge.wrapper.item.TRLItemStack;
 import dev.tr7zw.transliterationlib.forge.wrapper.util.TRLMatrixStack;
 import dev.tr7zw.transliterationlib.forge.wrapper.util.TRLVertexConsumerProvider;
 import me.shedaniel.clothconfig2.api.AbstractConfigEntry;
 import me.shedaniel.clothconfig2.gui.entries.EnumListEntry;
 import me.shedaniel.clothconfig2.gui.entries.IntegerSliderEntry;
+import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.RenderState;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.item.FilledMapItem;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.network.protocol.game.ClientboundSetEntityDataPacket;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CrossbowItem;
-import net.minecraft.world.storage.MapData;
+import net.minecraft.world.item.MapItem;
+import net.minecraft.world.level.saveddata.maps.MapItemSavedData;
 
 public class WrapperImpl implements OldWrapper{
 
@@ -96,55 +98,62 @@ public class WrapperImpl implements OldWrapper{
 	private static final RenderType MAP_BACKGROUND_CHECKERBOARD = getTextNoCull(new ResourceLocation("textures/map/map_background_checkerboard.png"));
 	
 	private static RenderType getTextNoCull(ResourceLocation texture) {
-		return RenderType.makeType("text", DefaultVertexFormats.POSITION_COLOR_TEX_LIGHTMAP, 7, 256, false, true,
-				RenderType.State.getBuilder().texture(new RenderState.TextureState(texture, false, false))
-						.alpha(RenderPhaseAlternative.ONE_TENTH_ALPHA)
-						.transparency(RenderPhaseAlternative.TRANSLUCENT_TRANSPARENCY)
-						.lightmap(RenderPhaseAlternative.ENABLE_LIGHTMAP).cull(RenderPhaseAlternative.DISABLE_CULLING)
-						.build(false));
+		return textNoCull.apply(texture);
 	}
 	
+	private static final Function<ResourceLocation, RenderType> textNoCull = Util.memoize(texture -> net.minecraft.client.renderer.RenderType.create("text", DefaultVertexFormat.POSITION_COLOR_TEX_LIGHTMAP,
+			VertexFormat.Mode.QUADS, 256, false, true,
+			RenderType.CompositeState.builder().setShaderState(RenderStateShard.RENDERTYPE_TEXT_SHADER)
+					.setTextureState(new RenderStateShard.TextureStateShard(texture, false, false)).setTransparencyState(RenderStateShard.TRANSLUCENT_TRANSPARENCY)
+					.setLightmapState(RenderStateShard.LIGHTMAP).setCullState(RenderStateShard.NO_CULL).createCompositeState(false)));
+	
 	@Override
-	public void renderFirstPersonMap(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light,
+	public void renderFirstPersonMap(MatrixStack matricesWrapped, VertexConsumerProvider vertexConsumersWrapped, int light,
 			ItemStack stack, boolean small, boolean lefthanded) {
-		com.mojang.blaze3d.matrix.MatrixStack matrixStackIn = ((TRLMatrixStack)matrices).handle();
-		IRenderTypeBuffer bufferIn = ((TRLVertexConsumerProvider)vertexConsumers).handle();
+		PoseStack matrices = ((TRLMatrixStack)matricesWrapped).handle();
+		MultiBufferSource vertexConsumers = ((TRLVertexConsumerProvider)vertexConsumersWrapped).handle();
 		Minecraft client = Minecraft.getInstance();
 
 		if (small) {
-			matrixStackIn.rotate(Vector3f.YP.rotationDegrees(160.0f));
-			matrixStackIn.rotate(Vector3f.ZP.rotationDegrees(180.0f));
-			matrixStackIn.scale(0.38f, 0.38f, 0.38f);
+			matrices.mulPose(Vector3f.YP.rotationDegrees(160.0f));
+			matrices.mulPose(Vector3f.ZP.rotationDegrees(180.0f));
+			matrices.scale(0.38f, 0.38f, 0.38f);
 			
-			matrixStackIn.translate(-0.1, -1.2, 0.0);
-			matrixStackIn.scale(0.0098125f, 0.0098125f, 0.0098125f);
+			matrices.translate(-0.1, -1.2, 0.0);
+			matrices.scale(0.0098125f, 0.0098125f, 0.0098125f);
 		} else {
 			if(lefthanded) {
-				matrixStackIn.rotate(Vector3f.YP.rotationDegrees(160.0f));
-				matrixStackIn.rotate(Vector3f.ZP.rotationDegrees(150.0f));
-				matrixStackIn.scale(0.38f, 0.38f, 0.38f);
+				matrices.mulPose(Vector3f.YP.rotationDegrees(160.0f));
+				matrices.mulPose(Vector3f.ZP.rotationDegrees(150.0f));
+				matrices.scale(0.38f, 0.38f, 0.38f);
 				
-				matrixStackIn.translate(+0.5, -1.3, 0.0);
+				matrices.translate(+0.5, -1.3, 0.0);
 			} else {
-				matrixStackIn.rotate(Vector3f.YP.rotationDegrees(160.0f));
-				matrixStackIn.rotate(Vector3f.ZP.rotationDegrees(210.0f));
-				matrixStackIn.scale(0.38f, 0.38f, 0.38f);
+				matrices.mulPose(Vector3f.YP.rotationDegrees(160.0f));
+				matrices.mulPose(Vector3f.ZP.rotationDegrees(210.0f));
+				matrices.scale(0.38f, 0.38f, 0.38f);
 				
-				matrixStackIn.translate(-1.0, -1.8, 0.0);
+				matrices.translate(-1.0, -1.8, 0.0);
 			}
 
-			matrixStackIn.scale(0.0138125f, 0.0138125f, 0.0138125f);
+			matrices.scale(0.0138125f, 0.0138125f, 0.0138125f);
 		}
-	      MapData mapdata = FilledMapItem.getMapData(((TRLItemStack) stack).handle(), client.world);
-	      IVertexBuilder ivertexbuilder = bufferIn.getBuffer(mapdata == null ? MAP_BACKGROUND : MAP_BACKGROUND_CHECKERBOARD);
-	      Matrix4f matrix4f = matrixStackIn.getLast().getMatrix();
-	      ivertexbuilder.pos(matrix4f, -7.0F, 135.0F, 0.0F).color(255, 255, 255, 255).tex(0.0F, 1.0F).lightmap(light).endVertex();
-	      ivertexbuilder.pos(matrix4f, 135.0F, 135.0F, 0.0F).color(255, 255, 255, 255).tex(1.0F, 1.0F).lightmap(light).endVertex();
-	      ivertexbuilder.pos(matrix4f, 135.0F, -7.0F, 0.0F).color(255, 255, 255, 255).tex(1.0F, 0.0F).lightmap(light).endVertex();
-	      ivertexbuilder.pos(matrix4f, -7.0F, -7.0F, 0.0F).color(255, 255, 255, 255).tex(0.0F, 0.0F).lightmap(light).endVertex();
-	      if (mapdata != null) {
-	    	  client.gameRenderer.getMapItemRenderer().renderMap(matrixStackIn, bufferIn, mapdata, false, light);
-	      }
+		Integer integer = MapItem.getMapId(((TRLItemStack) stack).handle());
+		MapItemSavedData mapState = MapItem.getSavedData(((TRLItemStack) stack).handle(), client.level);
+		com.mojang.blaze3d.vertex.VertexConsumer vertexConsumer = vertexConsumers
+				.getBuffer(mapState == null ? MAP_BACKGROUND : MAP_BACKGROUND_CHECKERBOARD);
+		Matrix4f matrix4f = matrices.last().pose();
+		vertexConsumer.vertex(matrix4f, -7.0f, 135.0f, 0.0f).color(255, 255, 255, 255).uv(0.0f, 1.0f).uv2(light)
+				.endVertex();
+		vertexConsumer.vertex(matrix4f, 135.0f, 135.0f, 0.0f).color(255, 255, 255, 255).uv(1.0f, 1.0f).uv2(light)
+				.endVertex();
+		vertexConsumer.vertex(matrix4f, 135.0f, -7.0f, 0.0f).color(255, 255, 255, 255).uv(1.0f, 0.0f).uv2(light)
+				.endVertex();
+		vertexConsumer.vertex(matrix4f, -7.0f, -7.0f, 0.0f).color(255, 255, 255, 255).uv(0.0f, 0.0f).uv2(light)
+				.endVertex();
+		if (mapState != null) {
+			client.gameRenderer.getMapRenderer().render(matrices, vertexConsumers, integer, mapState, false, light);
+		}
 	}
 	
 	@Override
